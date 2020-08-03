6adc26ae81352d35ee5675428fd34044990c0d94
mojtaba.eshghi <mojtaba.eshghi@ut.ac.ir>
2020-08-03 04:31:50 -0700

    adding the new multi-service app

--- 4/index.js
+++ 4/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const redis = require('redis')
 
 const app = express()
-const client = redis.createClient();
+const client = redis.createClient({
+    host: 'redis-server', // we can refer to host here by the service name we assigned at docker-compose
+    port: 6379 // default port redis works on
+});
 client.set('visits', 0);
 
 const PORT_NUMBER = 8080
@@ -10,10 +13,12 @@ const PORT_NUMBER = 8080
 
 app.get('/', (req, res) => {
     console.log("received a request");
+    
     client.get('visits', (err, visits) => {
-        res.send('Number of visits is ', visits);
+        res.send('Number of visits is ' + visits);
         client.set('visits', parseInt(visits) + 1);
     });
+
     console.log("just served the request.\n--------------------")
 });
 
